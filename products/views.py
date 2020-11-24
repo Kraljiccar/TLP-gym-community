e0cfabe40eb593b551319c077eda6a717586ece2
Kraljiccar <nemanjakraljic@gmail.com>
2020-11-24 19:21:07 +0000

    Had an error 404, after tutor help realised it was just cookies, added delete button in edir product

--- products/views.py
+++ products/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-
 from .models import Product, Category
 from .forms import ProductForm
 
 # Create your views here.
 
+
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to return all products page, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -31,18 +31,17 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -59,10 +58,9 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product details """
+    """ A view to preview product details """
 
     product = get_object_or_404(Product, pk=product_id)
-
     context = {
         'product': product,
     }
@@ -82,7 +80,7 @@ def add_product(request):
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
