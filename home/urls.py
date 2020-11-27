dd582f2a71f78f9c5a3ea6751a08d56103c89f0b
Kraljiccar <nemanjakraljic@gmail.com>
2020-11-27 19:10:54 +0000

    Added about page template, css, views and urls

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from home.views import AboutView
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
 
 # Create your views here.
 
@@ -7,3 +9,7 @@ def index(request):
     """ A view to return index page """
 
     return render(request, 'home/index.html')
+
+
+class AboutView(TemplateView):
+    template_name = "home/about.html"
