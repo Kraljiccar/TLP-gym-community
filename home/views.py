399cd7c0e2fcaeca08d5d74dc9690213afb0fa0c
Kraljiccar <nemanjakraljic@gmail.com>
2020-11-29 19:40:00 +0000

    Added setings, view and forms to contact page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
 from django.views import generic
 from django.views.generic import TemplateView
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -13,10 +17,6 @@ def index(request):
 
 class AboutView(TemplateView):
     template_name = "home/about.html"
-
-
-class ContactView(TemplateView):
-    template_name = "home/contact.html"
 
 
 class LoseweightView(TemplateView):
@@ -33,3 +33,33 @@ class EnduranceView(TemplateView):
 
 class LivehealthyView(TemplateView):
     template_name = "home/live-healthy.html"
+
+
+class ContactView(generic.FormView):
+    form_class = ContactForm
+    template_name = "home/contact.html"
+
+    def get_success_url(self):
+        return reverse("contact")
+
+    def form_valid(self, form):
+        """"Getting clean data from the form and creating
+        a message to get sent to default email."""
+        messages.success(self.request,
+                         "Thank you for getting in touch with us. We have received your message.")
+        name = form.cleaned_data.get('name')
+        email = form.cleaned_data.get('email')
+        message = form.cleaned_data.get('message')
+
+        full_message = f"""
+        Received message below from {name}, {email}
+        ___________________________
+        {message}
+        """
+        send_mail(
+            subject="Message from TLP contact form",
+            message=full_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.NOTIFY_EMAIL]
+        )
+        return super(ContactView, self).form_invalid(form)
--- tlp_gym_community/settings.py
+++ tlp_gym_community/settings.py
@@ -208,6 +208,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    NOTIFY_EMAIL = os.environ.get('NOTIFY_EMAIL')
     DEFAULT_FROM_EMAIL = 'tlpgymcommunity@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -217,3 +218,4 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    NOTIFY_EMAIL = os.environ.get('NOTIFY_EMAIL')
